3464fc0edc5ffc4a65df6198b6edcda6a9de9c7f
MidStrLK <stepma@list.ru>
2018-08-29 16:56:08 +0300

    Подсказка

--- src/app/buttons/button.component.ts
+++ src/app/buttons/button.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component,  } from '@angular/core';
 import { InteractionService } from '../service/interaction.service';
+
 
 @Component({
   selector:  'app-button-component',
@@ -7,10 +8,23 @@ import { InteractionService } from '../service/interaction.service';
   styleUrls: ['./button.component.css']
 })
 export class ButtonComponent {
+  is_show_tooltip = false;
+
   constructor(private interaction: InteractionService) {}
 
   /* Клик по кнопке добавления книги */
   onClickAdd() {
     this.interaction.modalNew();
   }
+
+  /* Показать тултип при наведении на инфо */
+  openTooltip(){
+    this.is_show_tooltip = true;
+  }
+
+  /* Скрыть тултип */
+  closeTooltip(){
+    this.is_show_tooltip = false;
+  }
+
 }
